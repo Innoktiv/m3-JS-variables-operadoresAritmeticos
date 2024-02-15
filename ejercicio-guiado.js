a480ebb8c93ca5c43c377a9f6af0578684f25f77
Claudio Dee <claudio.d@innkv.com>
2024-02-15 07:54:11 +0700

    Project started
    Files uploaded

---
+++ ejercicio-guiado.js
@@ -0,0 +1,24 @@
+var num1 = 1;
+var num2 = 2;
+
+
+var resultado_suma = num1 + num2
+var resultado_resta = num1 - num2
+var resultado_mutiplicacion = num1 + num2
+var resultado_division = num1 / num2
+
+
+console.log(resultado_suma)
+console.log(resultado_resta)
+console.log(resultado_mutiplicacion)
+console.log(resultado_division)
+
+
+
+const tempC = prompt("Temperatura en grados Celsiu: ")
+const tempK = tempC + 273.15
+
+console.log(tempK)
+
+const diasToAnos = prompt("Ingresa cantidad de dias: " )
+const resultado_anos = diasToAnos / 365
